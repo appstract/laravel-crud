5029cc7338a721bad2f56c870d14e2761e0b91c1
Gijs Jorissen <gizburdt@gmail.com>
2017-02-15 23:29:27 +0100

    ControllerMake

--- src/Console/ControllerMakeCommand.php
+++ src/Console/ControllerMakeCommand.php
@@ -2,7 +2,8 @@
 
 namespace Appstract\Crud\Console;
 
-use Symfony\Component\Console\Input\InputOption;
+use Illuminate\Support\Str;
+use InvalidArgumentException;
 
 class ControllerMakeCommand extends MakeCommand
 {
@@ -11,7 +12,7 @@ class ControllerMakeCommand extends MakeCommand
      *
      * @var string
      */
-    protected $signature = 'crud:controller';
+    protected $name = 'crud:controller';
 
     /**
      * The console command description.
@@ -30,12 +31,51 @@ class ControllerMakeCommand extends MakeCommand
     /**
      * Build the class with the given name.
      *
+     * Remove the base controller import if we are already in base namespace.
+     *
      * @param  string  $name
      * @return string
      */
     protected function buildClass($name)
     {
+        $controllerNamespace = $this->getNamespace($name);
+
+        $modelClass = $this->parseModel($this->option('model'));
+
+        $this->replace = [
+            'DummyFullModelClass' => $modelClass,
+            'DummyModelClass'     => class_basename($modelClass),
+            'DummyModelVariable'  => lcfirst(class_basename($modelClass)),
+            "use {$controllerNamespace}\Controller;\n" => '',
+
+            '{{table}}'      => $this->getTableName(),
+            '{{primaryKey}}' => $this->option('primary'),
+            '{{fillable}}'   => $this->parseFillable(),
+            '{{relations}}'  => $this->parseRelations()
+        ];
+
         return parent::buildClass($name);
+    }
+
+    /**
+     * Get the fully-qualified model class name.
+     *
+     * @param  string  $model
+     * @return string
+     */
+    protected function parseModel($model)
+    {
+        if (preg_match('([^A-Za-z0-9_/\\\\])', $model)) {
+            throw new InvalidArgumentException('Model name contains invalid characters.');
+        }
+
+        $model = trim(str_replace('/', '\\', $model), '\\');
+
+        if (! Str::startsWith($model, $rootNamespace = $this->laravel->getNamespace())) {
+            $model = $rootNamespace.$model;
+        }
+
+        return $model;
     }
 
     /**
@@ -46,6 +86,6 @@ class ControllerMakeCommand extends MakeCommand
      */
     protected function getDefaultNamespace($rootNamespace)
     {
-        return $rootNamespace;
+        return $rootNamespace.'\Http\Controllers';
     }
 }
